1c30ad17b3f651f1ece0845269a54d1a68dc651d
shalva97 <george0shalvashvili@gmail.com>
2026-07-08 19:41:42 +0400

    Add printHelloWorld function and tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.example
 
+fun printHelloWorld() {
+    println("Hello world")
+}
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
